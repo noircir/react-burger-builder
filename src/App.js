18b41c16ab311e7a038b066bfce0c2c1fa4eca4f
Nellie K <noircir@users.noreply.github.com>
2018-11-08 13:55:09 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+// import './App.css';
+import Layout from './components/Layout/Layout';
+
+class App extends Component {
+  render() {
+    return (
+      <Layout>
+        <div>
+        
+          <h1>Burger Builder</h1>
+
+        </div>
+      </Layout>
+    );
+  }
+}
+
+export default App;
